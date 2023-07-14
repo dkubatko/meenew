d5c86e4b4d9b7f0f1dec4eae4051df78b2fd0ea0
Daniel Kubatko <dankubatko@gmail.com>
2023-07-13 21:56:50 -0700

    Metadata & Home page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import Link from 'next/link';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Meenew | Home',
+  description: 'Welcome to Meenew!'
+}
 
 export default function Home() {
   return (
-    <div>
-      <Link href='/menu/123'>Sample menu!</Link>
-      <br/>
-      <Link href='http://127.0.0.1:8000/api/python'>Try the Python integration!</Link>
-    </div>
+    <>
+      <div className='list'>
+        <Link className='link' href='/restaurant/0'>Restaurant management demo!</Link>
+        <Link className='link' href='/menu/123'>Sample menu!</Link>
+        <Link className='link' href='http://127.0.0.1:8000/api/python'>Try the Python integration!</Link>
+      </div>
+    </>
   );
 }
--- src/app/restaurant/[id]/page.tsx
+++ src/app/restaurant/[id]/page.tsx
@@ -1,7 +1,20 @@
 import Restaurant from "@/app/components/owner/restaurant.component";
+import { Metadata } from "next";
+import Head from 'next/head';
+
+export const metadata: Metadata = {
+  title: 'Meenew | Restaurant',
+  description: 'Manage your restaurant.'
+}
 
 export default async function RestaurantPage() {
   return (
-    <Restaurant />
+    <>
+      <Head>
+        <title>HELLO</title>
+        <meta name='description' content='I hope this tutorial is helpful for you' />
+      </Head>
+      <Restaurant />
+    </>
   )
 }
