b785038ba5bfa74365239c934f351c24b7ef143b
Daniel Kubatko <dankubatko@gmail.com>
2023-08-10 18:30:38 -0700

    Add categories table that assigns a category to menu items.

--- api/database/models.py
+++ api/database/models.py
@@ -26,7 +26,9 @@ class MenuItem(MenuItemBase, table = True):
     id: Optional[int] = Field(default=None, primary_key=True, index=True)
     restaurant_id: Optional[int] = Field(default = None, foreign_key="restaurants.id")
     image_path: Optional[str] = Field(default = None)
+    category_id: Optional[int] = Field(default=None, foreign_key="categories.id")
 
+    category: Optional["Category"] = Relationship(back_populates="menu_items")
     restaurant: Optional[Restaurant] = Relationship(back_populates="menu_items")
     tags: List["Tag"] = Relationship(back_populates="menu_items", link_model=ItemTagLink)
 
@@ -85,3 +87,26 @@ class MenuItemCreate(MenuItemBase):
     restaurant_id: int
     image_path: Optional[str]
     tags: List["TagRead"]
+
+class CategoryBase(SQLModel):
+    name: str = Field(unique=True, index=True)
+
+class Category(CategoryBase, table=True):
+    __tablename__: str = "categories"
+
+    id: Optional[int] = Field(default=None, primary_key=True, index=True, nullable=False)
+    parent_id: Optional[int] = Field(default=None, foreign_key="categories.id", nullable=True)
+
+    parent: Optional["Category"] = Relationship(
+        back_populates="children",
+        sa_relationship_kwargs=dict(remote_side="Category.id")
+    )
+    children: List["Category"] = Relationship(back_populates="parent")
+    menu_items: List["MenuItem"] = Relationship(back_populates="category")
+
+class CategoryRead(CategoryBase):
+    id: int
+    parent_id: Optional[int]
+
+class CategoryCreate(CategoryBase):
+    parent_id: int
