af49ac5b79675f5de6d46eaf4efeaeae68da9c4d
Daniel Kubatko <dankubatko@gmail.com>
2023-08-20 12:09:05 -0700

    Include all parent tag labels as well as menu items in the branch for category requests

--- api/database/crud/category.py
+++ api/database/crud/category.py
@@ -1,10 +1,10 @@
 from sqlmodel import Session, select
 from fastapi import HTTPException
 from .base import CRUDBase
-from ..models import Category
+from ..models import Category, CategoryTreeRead, CategoryLite, CategoryRead
 
 class CategoryCRUD(CRUDBase):
-    def get(self, restaurant_id: int, category_id: int):
+      def get(self, restaurant_id: int, category_id: int):
         category = self.db.query(Category).filter(
             Category.id == category_id,
             Category.restaurant_id == restaurant_id
@@ -13,4 +13,46 @@ class CategoryCRUD(CRUDBase):
         if not category:
             raise HTTPException(status_code=404, detail=f"Category w/ id = {category_id} and restaurant_id = {restaurant_id} not found.")
 
-        return category
+        # Function to recursively get all menu items for a category
+        def get_all_menu_items(cat):
+            menu_items = [item for item in cat.menu_items]
+            for child in cat.children:
+                menu_items.extend(get_all_menu_items(child))
+            return menu_items
+
+        # Function to get all tag labels up the tree
+        def get_all_tag_labels(cat):
+            tag_labels = [tag_label for tag_label in cat.tag_labels]
+            parent = cat.parent
+            while parent:
+                tag_labels.extend(parent.tag_labels)
+                parent = parent.parent
+            return tag_labels
+
+        # Create CategoryRead for children
+        children = []
+        for child in category.children:
+            children.append(
+                CategoryRead(
+                    name=child.name,
+                    restaurant_id=restaurant_id,
+                    id=child.id,
+                    menu_items=get_all_menu_items(child),
+                    tag_labels=child.tag_labels,
+                    parent=CategoryLite(id=child.parent_id,
+                                        restaurant_id=restaurant_id,
+                                        name=child.parent.name if child.parent else None)
+                )
+            )
+
+        # Create the CategoryTreeRead for the requested category
+        return CategoryTreeRead(
+            name=category.name,
+            restaurant_id=restaurant_id,
+            id=category.id,
+            parent=category.parent,
+            menu_items=category.menu_items,
+            children=children,
+            tag_labels=get_all_tag_labels(category)
+        )
+
